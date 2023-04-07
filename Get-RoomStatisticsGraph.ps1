9bf37ecfffc2962a3785e7a1407331e7105e9d25
Andres Bohren <a.bohren@icewolf.ch>
2023-04-07 16:56:08 +0200

    Updates 20230407

--- Get-RoomStatisticsGraph.ps1
+++ Get-RoomStatisticsGraph.ps1
@@ -31,16 +31,16 @@ param (
 ###############################################################################
 $TenantId = "icewolfch.onmicrosoft.com"
 $Scope = "https://graph.microsoft.com/.default" 
-$AppID = "c1a5903b-cd73-48fe-ac1f-e71bde968412" #DelegatedMail
-$CertificateThumbprint = "07EFF3918F47995EB53B91848F69B5C0E78622FD" #O365Powershell3.cer
+$AppID = "fe8d0ffa-5f63-4dd6-b0ee-19ae62da4dcc" #RoomStatisticsGraph
+$CertificateThumbprint = "BE6096887FDC1C12438E0477EABAB36953A3A26C" #O365Powershell3.cer
 
 ###############################################################################
 # Connect to Exchange Online
 ###############################################################################
-$ConnInfo  = Get-ConnectionInformation
+$ConnInfo  = Get-ConnectionInformation -ErrorAction SilentlyContinue
 If ($Null -eq $ConnInfo) {
 	Write-Host "Connect to Exchange Online" -ForegroundColor green
-    Connect-ExchangeOnline ShowBanner:$false
+    Connect-ExchangeOnline -ShowBanner:$false
 	#Connect-ExchangeOnline -CertificateThumbprint $CertificateThumbprint -AppID $AppID -Organization $TenantId	
 }Else {
     Write-Host "Connection to Exchange Online already exists" -ForegroundColor yellow
---
+++ Register-RoomStatisticsGraphApp.ps1
@@ -0,0 +1,111 @@
+###############################################################################
+#Connect-MgGraph
+#Connect to your Azure Active Directory with "Application Adminstrator" or "Global Administrator" Role
+###############################################################################
+
+
+param (
+    [Parameter(Mandatory = $true, ParameterSetName = "CreateAzureApplication")]
+    [Switch]$CreateAzureApplication,
+
+    [Parameter(Mandatory = $true, ParameterSetName = "DeleteAzureApplication")]
+    [Switch]$DeleteAzureApplication
+)
+
+If ($CreateAzureApplication)
+{
+	#Connect-MgGraph
+	Disconnect-MgGraph -ErrorAction SilentlyContinue
+	Connect-MgGraph -Scopes "Application.Read.All","Application.ReadWrite.All","User.Read.All","AppRoleAssignment.ReadWrite.All"
+	Get-MgContext
+
+	#Create AAD Application
+	$AppName =  "RoomStatisticsGraph"
+	$App = New-MgApplication -DisplayName $AppName 
+	$APPObjectID = $App.Id
+
+	#Create a Self Signed Certificate
+	$Subject = $AppName
+	$NotAfter = (Get-Date).AddMonths(+24)
+	$Cert = New-SelfSignedCertificate -Subject $Subject -CertStoreLocation "Cert:\CurrentUser\My" -KeySpec Signature -NotAfter $Notafter -KeyExportPolicy Exportable
+	#$ThumbPrint = $Cert.ThumbPrint
+
+	<#
+	#View Certificates in the Current User Certificate Store
+	Get-ChildItem -Path cert:\CurrentUser\my\$ThumbPrint | Format-Table
+
+	#Export Certificate as Base64 (PEM Format)
+	$CurrentLocation = (Get-Location).path
+	$Base64 = [convert]::tobase64string((get-item cert:\currentuser\my\$ThumbPrint).RawData)
+	$Base64Block = $Base64 |
+	ForEach-Object {
+		$line = $_
+
+		for ($i = 0; $i -lt $Base64.Length; $i += 64)
+		{
+			$length = [Math]::Min(64, $line.Length - $i)
+			$line.SubString($i, $length)
+		}
+	}
+	$base64Block2 = $Base64Block | Out-String
+
+	$Value = "-----BEGIN CERTIFICATE-----`r`n"
+	$Value += "$Base64Block2"
+	$Value += "-----END CERTIFICATE-----"
+	$Value
+	Set-Content -Path "$CurrentLocation\$Subject-BASE64.cer" -Value $Value
+	#>
+
+	#Add Certificate to AzureAD App
+	$keyCreds = @{ 
+		Type = "AsymmetricX509Cert";
+		Usage = "Verify";
+		key = $cert.RawData
+	}
+	
+	try {
+	Update-MgApplication -ApplicationId $APPObjectID  -KeyCredentials $keyCreds
+	} catch {
+	Write-Error $Error[0]
+	}
+
+	#Add Application Permissions
+	#Calendars.Read	Application	798ee544-9d2d-430c-a058-570e29e34338
+	#Calendars.ReadBasic.All	Application	8ba4a692-bc31-4128-9094-475872af8a53
+	$params = @{
+		RequiredResourceAccess = @(
+			@{
+				ResourceAppId = "00000003-0000-0000-c000-000000000000"
+				ResourceAccess = @(
+					@{
+						Id = "798ee544-9d2d-430c-a058-570e29e34338"
+						Type = "Role"
+					}
+				)
+			}
+		)
+	}
+	Update-MgApplication -ApplicationId $APPObjectID -BodyParameter $params
+
+	#Grant Permission
+	$ServicePrincipal = Get-MgServicePrincipal -Filter "DisplayName eq 'RoomStatisticsGraph'"
+	$params = @{
+		PrincipalId = "bb6c9bfd-7426-40ea-9292-0013f75a7e85"
+		ResourceId = "00000003-0000-0000-c000-000000000000"
+		AppRoleId = "798ee544-9d2d-430c-a058-570e29e34338"
+	}
+	New-MgServicePrincipalAppRoleAssignment -ServicePrincipalId $servicePrincipalId -BodyParameter $params
+
+	#Add Redirect URI
+	$App = Get-MgApplication -ApplicationId $APPObjectID -Property * 
+	$AppId = $App.AppId
+	$RedirectURI = @()
+	$RedirectURI += "https://login.microsoftonline.com/common/oauth2/nativeclient"
+	$RedirectURI += "msal" + $AppId + "://auth"
+
+	$params = @{
+		RedirectUris = @($RedirectURI)
+	}
+	Update-MgApplication -ApplicationId $APPObjectID -IsFallbackPublicClient -PublicClient $params
+
+}
